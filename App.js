c8e2edc912815accce9dad86db609b6749ae7ddc
Paul Rail <paul.a.rail@gmail.com>
2019-06-06 15:36:00 -0400

    Platform component to have styles based on OS Android or iOS

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Platform } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
+    marginTop: Platform.OS === "android" ? 24 : 0,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-});
+};
