ed6232916359343f374ed1d56aa789c53406f140
Paul Rail <paul.a.rail@gmail.com>
2019-06-07 12:12:00 -0400

    background image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Platform } from 'react-native';
+import { Text, View, Platform, ImageBackground } from 'react-native';
 import { Button } from 'native-base';
 
 // export default function App() {
@@ -9,13 +9,18 @@ import { Button } from 'native-base';
 //     </View>
 //   );
 // }
+
+let myBackground = require('./assets/landing.jpg');
+
 export default class App extends React.Component {
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Button><Text>   Hello World   </Text></Button>
+        <ImageBackground source={myBackground} style={{height: '100%', width: '100%'}}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Button><Text>   Hello World   </Text></Button>
+        </ImageBackground>
       </View>
     );
   }
